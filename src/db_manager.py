381a20741a17717d44b6c34cd2985fdd43f41567
User1 <431410@mail.ru>
2024-11-19 00:22:04 +0900

    create db_manager

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,14 +1,69 @@
-from src.models import Problem
+from src.models import Problem, Base
+from sqlalchemy.exc import IntegrityError
 
 
-class DBManager:
-    """
-    Класс для работы с данными в БД
-    """
+def create_table(engine):
+    """Создание всех таблиц в базе данных"""
 
-    def get_problems_with_theme(self, session, theme, rating):
-        """Получает список всех задач, по указанной теме и сложности"""
+    Base.metadata.create_all(engine)
 
-        problems = session.query(Problem).filter(Problem.tags.ilike(f'%{theme}%'), Problem.rating == rating).all()
 
-        return problems
+def save_data_problems(objects, session):
+    """Заполнение базы данных"""
+
+    for problem in objects:
+        try:
+            new_problem = Problem(
+                contest_id=problem.contest_id,
+                index=problem.index,
+                name=problem.name,
+                tags=(', '.join(problem.tags)),
+                rating=problem.rating,
+                solved_count=problem.solved_count
+            )
+            session.add(new_problem)
+            session.commit()
+        except IntegrityError:
+            session.rollback()
+            continue
+
+    session.close()
+
+
+def get_themes_list(session):
+    """Получает список всех тем задач"""
+
+    themes = session.query(Problem.tags).all()
+    themes_list = []
+
+    for item in themes:
+        theme = ', '.join(item)
+        themes_list.extend(theme.split(', '))
+
+    session.close()
+
+    return set(themes_list)
+
+
+def get_rating_list(session):
+    """Получает список сложности задач"""
+
+    ratings = session.query(Problem.rating).all()
+    rating_list = []
+
+    for i in ratings:
+        rating_list.append(i)
+
+    session.close()
+
+    return set(rating_list)
+
+
+def get_problems_with_theme_and_rating(session, theme, rating):
+    """Получает список всех задач, по указанной теме и сложности"""
+
+    problems_filter = session.query(Problem).filter(Problem.tags.ilike(f'%{theme}%'), Problem.rating == rating).all()
+
+    session.close()
+
+    return problems_filter
