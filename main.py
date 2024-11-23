1dea860dc8aa899d73ecee03be4cc0d8b0468108
User1 <431410@mail.ru>
2024-11-23 13:48:26 +0900

    corrected

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from src.db_manager import create_table, save_data_problems
 from src.functions import main
-from src.utils import DATABASE_URL
+from src.utils import session, engine
 
-
-engine = create_engine(DATABASE_URL)
-
-Session = sessionmaker(bind=engine)
-session = Session()
 
 if __name__ == '__main__':
     main(engine, session)
