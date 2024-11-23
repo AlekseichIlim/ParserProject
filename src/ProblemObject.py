ef3943393cc9a8446c6738a2dff916a682fb09ed
User1 <431410@mail.ru>
2024-11-23 12:35:20 +0900

    corrected

--- src/ProblemObject.py
+++ src/ProblemObject.py
@@ -1,5 +1,8 @@
+from src.db_manager import get_problems_with_theme_and_rating
+
+
 class ProblemObject:
-    def __init__(self,  index, name, tags, solved_count, contest_id, rating):
+    def __init__(self,  contest_id, index, name, tags, rating, solved_count):
 
         self.index = index
         self.name = name
@@ -20,10 +23,25 @@ class ProblemObject:
         for problem in list_problems:
             if problem.get('rating'):
                 object_list.append(
-                    cls(problem['index'], problem['name'], problem['tags'], problem['solvedCount'],
-                        problem['contestId'], problem['rating']))
+                    cls(problem['contestId'], problem['index'], problem['name'], problem['tags'], problem['rating'],
+                        problem['solvedCount']))
             else:
                 object_list.append(
-                    cls(problem['index'], problem['name'], problem['tags'], problem['solvedCount'],
-                        problem['contestId'], None))
+                    cls(problem['contestId'], problem['index'], problem['name'], problem['tags'], None,
+                        problem['solvedCount']))
         return object_list
+
+    @classmethod
+    def get_to_compilation(cls, problems_filter_list):
+        """
+        Возвращает подборку объектов отсортированных задач
+        """
+
+        object_list = []
+        for problem in problems_filter_list:
+            object_list.append(
+                cls(problem.contest_id, problem.index, problem.name, problem.tags, problem.rating,
+                    problem.solved_count))
+
+        return object_list
+
